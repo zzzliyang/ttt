d2e296898aff3adb6cddd3e2ff36e6f35fb30fa2
lzhang <Liyang.Zhang@navikcapital.com>
2020-03-02 19:14:17 +0800

    basic layout;docs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,41 +2,18 @@ import React, { Component } from 'react';
 import { BackTop, Row, Col, Divider } from 'antd';
 import '../css/App.css';
 import NavikHeader from './NavikHeader';
-import ControlPanel from './ControlPanel';
-import PivotTable from './PivotTable';
-
-const rows = ['portfolio', 'strategy'];
-
-const vals = ['mtd_usd'];
-
-const aggregatorName = 'Sum';
-
-const initialState = {
-  rows: rows,
-  vals: vals,
-  aggregatorName: aggregatorName
-};
+import FormSteps from './FormSteps';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false,
-      navikdata: [],
-      mlpdata: []
+      loading: false
     };
   }
 
   setLoading = (loading) => {
     this.setState({ loading: loading });
-  }
-
-  setNavikdata = (data) => {
-    this.setState({ navikdata: data });
-  }
-
-  setMlpdata = (data) => {
-    this.setState({ mlpdata: data });
   }
 
   render() {
@@ -51,27 +28,7 @@ class App extends Component {
         <BackTop />
         <NavikHeader />
         <Row>
-          <ControlPanel
-            controlPanelState={controlPanelState}
-          />
-        </Row>
-        <Row style={{minHeight: '800px', marginLeft:'20px'}}>
-          <Col span={12} style={{overflowX: 'auto'}}>
-            <Divider >MLP</Divider>
-            <PivotTable
-              loading={this.state.loading}
-              data={this.state.navikdata}
-              initialState={initialState}
-            />
-          </Col>
-          <Col span={12} style={{overflowX: 'auto'}}>
-            <Divider >NAVIK</Divider>
-            <PivotTable
-              loading={this.state.loading}
-              data={this.state.mlpdata}
-              initialState={initialState}
-            />
-          </Col>
+          <FormSteps/>
         </Row>
       </div>
     );
